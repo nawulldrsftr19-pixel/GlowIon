e5a6e47e4e94af3592c5f1f11f0f1ee617dea5ba
nawulldrsftr19-pixel <nawulldrsftr19@gmail.com>
2026-06-07 19:49:47 +0700

    Update GlowIon_app.py

--- GlowIon_app.py
+++ GlowIon_app.py
@@ -174,7 +174,7 @@ with tab3:
         </div>
         """, unsafe_allow_html=True)
         
-elif anion=="Sulfat (SO₄²⁻)":
+    elif anion=="Sulfat (SO₄²⁻)":
         st.latex(r"SO_4^{2-} + BaCl_2 \rightarrow BaSO_4(s) \downarrow + 2Cl^-")
         st.write("Uji: terbentuk endapan putih BaSO₄ yang tidak larut dalam asam.")
         tube_viz("lightblue","white",35)
